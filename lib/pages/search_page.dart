c986a448f1e137823555d869e04b72ed74e9d9d2
Mehrob02 <mehrobjonabdulvahobov@gmail.com>
2025-12-19 14:53:38 +0500

    fix: searchBar focus

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -17,6 +17,7 @@ class _SearchPageState extends State<SearchPage> {
   List<Literature> books = [];
   Filter? filter;
   TextEditingController textEditingController = TextEditingController();
+  FocusNode myFocusNode = FocusNode();
 
   void search() {
     final query = textEditingController.text.trim();
@@ -34,78 +35,90 @@ class _SearchPageState extends State<SearchPage> {
   @override
   void dispose() {
     textEditingController.dispose();
+    myFocusNode.dispose();
     super.dispose();
   }
+
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Поиск"),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              final result = await showModalBottomSheet<Filter>(
-                context: context,
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-                ),
-                builder: (_) => FilterSheet(current: filter),
-              );
-
-              if (result != null) {
-                setState(() {
-                  filter = result;
-                });
-                search();
-              }
-            },
-            icon: Icon(Icons.filter_alt_rounded),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          spacing: 10,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Hero(
-                    tag: "search",
-                    child: TextField(
-                      onSubmitted: (_) => search(),
-                      controller: textEditingController,
-                    ),
+    return WillPopScope(
+      onWillPop: () async {
+        if (myFocusNode.hasFocus) {
+          myFocusNode.unfocus();
+          return false;
+        }
+        return true;
+        },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Поиск"),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                final result = await showModalBottomSheet<Filter>(
+                  context: context,
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
                   ),
-                ),
-                IconButton(onPressed: search, icon: Icon(Icons.search)),
-              ],
+                  builder: (_) => FilterSheet(current: filter),
+                );
+      
+                if (result != null) {
+                  setState(() {
+                    filter = result;
+                  });
+                  search();
+                }
+              },
+              icon: Icon(Icons.filter_alt_rounded),
             ),
-            books.isEmpty
-                ? Expanded(child: Center(child: Text("Ничего не найдено")))
-                : Expanded(
-                  child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount:
-                          width > 800
-                              ? 4
-                              : width > 600
-                              ? 3
-                              : width > 400
-                              ? 2
-                              : 1,
-                    ),
-                    shrinkWrap: true,
-                    itemCount: books.length,
-                    itemBuilder:
-                        (context, index) => BookCard(book: books[index]),
-                  ),
-                ),
           ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            spacing: 10,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Hero(
+                      tag: "search",
+                      child: TextField(
+                        onSubmitted: (_) => search(),
+                        controller: textEditingController,
+                        focusNode: myFocusNode,
+                      ),
+                    ),
+                  ),
+                  IconButton(onPressed: search, icon: Icon(Icons.search)),
+                ],
+              ),
+              books.isEmpty
+                  ? Expanded(child: Center(child: Text("Ничего не найдено")))
+                  : Expanded(
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount:
+                            width > 800
+                                ? 4
+                                : width > 600
+                                ? 3
+                                : width > 400
+                                ? 2
+                                : 1,
+                      ),
+                      shrinkWrap: true,
+                      itemCount: books.length,
+                      itemBuilder:
+                          (context, index) => BookCard(book: books[index]),
+                    ),
+                  ),
+            ],
+          ),
         ),
       ),
     );
